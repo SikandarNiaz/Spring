e943e90724e1ed71f537b445cc0ffdafd0c2e9f7
Sikandar Niaz <sikandar.niaz@marksman.com.pk>
2024-02-18 13:26:01 +0500

    initial commit

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,172 @@
+import { Component, OnInit } from "@angular/core";
+import { NgbDropdownConfig } from "@ng-bootstrap/ng-bootstrap";
+import { MegaMenuItem } from "primeng/api";
+
+@Component({
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
+  providers: [NgbDropdownConfig],
+})
+export class NavbarComponent implements OnInit {
+  public iconOnlyToggled = false;
+  public sidebarToggled = false;
+  items: MegaMenuItem[] | undefined;
+
+  constructor(config: NgbDropdownConfig) {
+    config.placement = "bottom-right";
+  }
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: "Videos",
+        icon: "pi pi-fw pi-video",
+        items: [
+          [
+            {
+              label: "Video 1",
+              items: [{ label: "Video 1.1" }, { label: "Video 1.2" }],
+            },
+            {
+              label: "Video 2",
+              items: [{ label: "Video 2.1" }, { label: "Video 2.2" }],
+            },
+          ],
+          [
+            {
+              label: "Video 3",
+              items: [{ label: "Video 3.1" }, { label: "Video 3.2" }],
+            },
+            {
+              label: "Video 4",
+              items: [{ label: "Video 4.1" }, { label: "Video 4.2" }],
+            },
+          ],
+        ],
+      },
+
+      {
+        label: "Users",
+        icon: "pi pi-fw pi-users",
+        items: [
+          [
+            {
+              label: "User 1",
+              items: [{ label: "User 1.1" }, { label: "User 1.2" }],
+            },
+            {
+              label: "User 2",
+              items: [{ label: "User 2.1" }, { label: "User 2.2" }],
+            },
+          ],
+          [
+            {
+              label: "User 3",
+              items: [{ label: "User 3.1" }, { label: "User 3.2" }],
+            },
+            {
+              label: "User 4",
+              items: [{ label: "User 4.1" }, { label: "User 4.2" }],
+            },
+          ],
+          [
+            {
+              label: "User 5",
+              items: [{ label: "User 5.1" }, { label: "User 5.2" }],
+            },
+            {
+              label: "User 6",
+              items: [{ label: "User 6.1" }, { label: "User 6.2" }],
+            },
+          ],
+        ],
+      },
+      {
+        label: "Events",
+        icon: "pi pi-fw pi-calendar",
+        items: [
+          [
+            {
+              label: "Event 1",
+              items: [{ label: "Event 1.1" }, { label: "Event 1.2" }],
+            },
+            {
+              label: "Event 2",
+              items: [{ label: "Event 2.1" }, { label: "Event 2.2" }],
+            },
+          ],
+          [
+            {
+              label: "Event 3",
+              items: [{ label: "Event 3.1" }, { label: "Event 3.2" }],
+            },
+            {
+              label: "Event 4",
+              items: [{ label: "Event 4.1" }, { label: "Event 4.2" }],
+            },
+          ],
+        ],
+      },
+      {
+        label: "Settings",
+        icon: "pi pi-fw pi-cog",
+        items: [
+          [
+            {
+              label: "Setting 1",
+              items: [{ label: "Setting 1.1" }, { label: "Setting 1.2" }],
+            },
+            {
+              label: "Setting 2",
+              items: [{ label: "Setting 2.1" }, { label: "Setting 2.2" }],
+            },
+            {
+              label: "Setting 3",
+              items: [{ label: "Setting 3.1" }, { label: "Setting 3.2" }],
+            },
+          ],
+          [
+            {
+              label: "Technology 4",
+              items: [{ label: "Setting 4.1" }, { label: "Setting 4.2" }],
+            },
+          ],
+        ],
+      },
+    ];
+  }
+
+  // toggle sidebar in small devices
+  toggleOffcanvas() {
+    document.querySelector(".sidebar-offcanvas").classList.toggle("active");
+  }
+
+  // toggle sidebar
+  toggleSidebar() {
+    let body = document.querySelector("body");
+    if (
+      !body.classList.contains("sidebar-toggle-display") &&
+      !body.classList.contains("sidebar-absolute")
+    ) {
+      this.iconOnlyToggled = !this.iconOnlyToggled;
+      if (this.iconOnlyToggled) {
+        body.classList.add("sidebar-icon-only");
+      } else {
+        body.classList.remove("sidebar-icon-only");
+      }
+    } else {
+      this.sidebarToggled = !this.sidebarToggled;
+      if (this.sidebarToggled) {
+        body.classList.add("sidebar-hidden");
+      } else {
+        body.classList.remove("sidebar-hidden");
+      }
+    }
+  }
+
+  // toggle right sidebar
+  // toggleRightSidebar() {
+  //   document.querySelector('#right-sidebar').classList.toggle('open');
+  // }
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,150 @@
+import { Component, OnInit } from "@angular/core";
+import { MegaMenuItem } from "primeng/api";
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
+})
+export class SidebarComponent implements OnInit {
+  public uiBasicCollapsed = false;
+  public samplePagesCollapsed = false;
+  items: MegaMenuItem[] | undefined;
+  vertical: any;
+
+  constructor() {}
+
+  ngOnInit() {
+    const body = document.querySelector("body");
+
+    // add class 'hover-open' to sidebar navitem while hover in sidebar-icon-only menu
+    document.querySelectorAll(".sidebar .nav-item").forEach(function (el) {
+      el.addEventListener("mouseover", function () {
+        if (body.classList.contains("sidebar-icon-only")) {
+          el.classList.add("hover-open");
+        }
+      });
+      el.addEventListener("mouseout", function () {
+        if (body.classList.contains("sidebar-icon-only")) {
+          el.classList.remove("hover-open");
+        }
+      });
+    });
+    this.items = [
+      {
+        label: "Reports",
+        icon: "pi pi-envelope",
+        items: [
+          [
+            {
+              label: "Report 1",
+              items: [{ label: "Report 1.1" }, { label: "Report 1.2" }],
+            },
+            {
+              label: "Report 2",
+              items: [{ label: "Report 2.1" }, { label: "Report 2.2" }],
+            },
+          ],
+          [
+            {
+              label: "Report 3",
+              items: [{ label: "Report 3.1" }, { label: "Report 3.2" }],
+            },
+            {
+              label: "Report 4",
+              items: [{ label: "Report 4.1" }, { label: "Report 4.2" }],
+            },
+          ],
+        ],
+      },
+      {
+        label: "Dashboards",
+        icon: "pi pi-chart-line",
+        items: [
+          [
+            {
+              label: "Dashboard 1",
+              items: [{ label: "Dashboard 1.1" }, { label: "Dashboard 1.2" }],
+            },
+            {
+              label: "Dashboard 2",
+              items: [{ label: "Dashboard 2.1" }, { label: "Dashboard 2.2" }],
+            },
+          ],
+          [
+            {
+              label: "Dashboard 3",
+              items: [{ label: "Dashboard 3.1" }, { label: "Dashboard 3.2" }],
+            },
+            {
+              label: "Dashboard 4",
+              items: [{ label: "Dashboard 4.1" }, { label: "Dashboard 4.2" }],
+            },
+          ],
+          [
+            {
+              label: "Dashboard 5",
+              items: [{ label: "Dashboard 5.1" }, { label: "Dashboard 5.2" }],
+            },
+            {
+              label: "Dashboard 6",
+              items: [{ label: "Dashboard 6.1" }, { label: "Dashboard 6.2" }],
+            },
+          ],
+        ],
+      },
+      {
+        label: "Finance",
+        icon: "pi pi-chart-bar",
+        items: [
+          [
+            {
+              label: "Finance 1",
+              items: [{ label: "Finance 1.1" }, { label: "Finance 1.2" }],
+            },
+            {
+              label: "Finance 2",
+              items: [{ label: "Finance 2.1" }, { label: "Finance 2.2" }],
+            },
+          ],
+          [
+            {
+              label: "Finance 3",
+              items: [{ label: "Finance 3.1" }, { label: "Finance 3.2" }],
+            },
+            {
+              label: "Finance 4",
+              items: [{ label: "Finance 4.1" }, { label: "Finance 4.2" }],
+            },
+          ],
+        ],
+      },
+      {
+        label: "Settings",
+        icon: "pi pi-fw pi-cog",
+        items: [
+          [
+            {
+              label: "Setting 1",
+              items: [{ label: "Setting 1.1" }, { label: "Setting 1.2" }],
+            },
+            {
+              label: "Setting 2",
+              items: [{ label: "Setting 2.1" }, { label: "Setting 2.2" }],
+            },
+            {
+              label: "Setting 3",
+              items: [{ label: "Setting 3.1" }, { label: "Setting 3.2" }],
+            },
+          ],
+          [
+            {
+              label: "Technology 4",
+              items: [{ label: "Setting 4.1" }, { label: "Setting 4.2" }],
+            },
+          ],
+        ],
+      },
+    ];
+  }
+}
